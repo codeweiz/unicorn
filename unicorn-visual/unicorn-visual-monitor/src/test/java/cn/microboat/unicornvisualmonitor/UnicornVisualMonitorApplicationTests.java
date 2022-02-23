6a86e7fee890898332153f96c022be1ba4657926
microboat <13955645241@163.com>
2022-02-23 09:46:16 +0800

    项目架构完善

---
+++ unicorn-visual/unicorn-visual-monitor/src/test/java/cn/microboat/unicornvisualmonitor/UnicornVisualMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.microboat.unicornvisualmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicornVisualMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
